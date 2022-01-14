4e39415533ef734cf7c3d41f95242e47f7e64dc9
dhoonjang <dhoonjang@gmail.com>
2022-01-15 05:03:49 +0900

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,7 @@ import router from './api';
 const port = process.env.PORT || 8080;
 const app = express();
 
-const whitelist = [
-  'http://localhost:8080',
-  'https://dhoonjang.github.io/jerk-lol',
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (origin && whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not Allowed Origin!'));
-      }
-    },
-  })
-);
+app.use(cors());
 
 app.use('/api', router);
 
