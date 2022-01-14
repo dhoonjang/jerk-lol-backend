9da214b782a10717ade9659a8de9441f0b650b34
dhoonjang <dhoonjang@gmail.com>
2022-01-15 04:16:08 +0900

    feat: cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import router from './api';
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+const whitelist = [
+  'http://localhost:8080',
+  'https://dhoonjang.github.io/jerk-lol',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (origin && whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not Allowed Origin!'));
+      }
+    },
+  })
+);
 
 app.use('/api', router);
 
